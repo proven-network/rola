90173594145d726d747bb705fa964316e1b1ac6c
flightofthefox <foxy@borderlinelabs.com>
2024-05-31 18:57:07 +0800

    Handle publicKey variants.

--- src/challenge.rs
+++ src/challenge.rs
@@ -1,5 +1,5 @@
-use radix_common::crypto::PublicKey;
-use serde::Deserialize;
+use radix_common::crypto::{Ed25519PublicKey, PublicKey, Secp256k1PublicKey};
+use serde::{Deserialize, Deserializer};
 
 #[derive(Clone, Debug, Deserialize, PartialEq)]
 #[serde(rename_all = "camelCase")]
@@ -15,8 +15,7 @@ pub enum Type {
     Persona,
 }
 
-#[derive(Clone, Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
+#[derive(Clone, Debug)]
 pub struct Proof {
     pub curve: Curve,
     pub public_key: PublicKey,
@@ -30,4 +29,53 @@ pub struct SignedChallenge {
     pub challenge: String,
     pub proof: Proof,
     pub r#type: Type,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct RawProof {
+    curve: String,
+    public_key: String,
+    signature: String,
+}
+
+impl TryFrom<RawProof> for Proof {
+    type Error = Box<dyn std::error::Error>;
+
+    fn try_from(raw: RawProof) -> Result<Self, Self::Error> {
+        let curve = match raw.curve.as_str() {
+            "curve25519" => Curve::Curve25519,
+            "secp256k1" => Curve::Secp256k1,
+            _ => return Err("Invalid curve".into()),
+        };
+
+        let bytes = hex::decode(&raw.public_key)?;
+
+        let public_key = match curve {
+            Curve::Curve25519 => {
+                let key = Ed25519PublicKey::try_from(bytes.as_slice())?;
+                PublicKey::Ed25519(key)
+            }
+            Curve::Secp256k1 => {
+                let key = Secp256k1PublicKey::try_from(bytes.as_slice())?;
+                PublicKey::Secp256k1(key)
+            }
+        };
+
+        Ok(Proof {
+            curve,
+            public_key,
+            signature: raw.signature,
+        })
+    }
+}
+
+impl<'de> Deserialize<'de> for Proof {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let raw = RawProof::deserialize(deserializer)?;
+        Proof::try_from(raw).map_err(serde::de::Error::custom)
+    }
 }
